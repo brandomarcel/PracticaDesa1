f9885cf8f325fbfb5c196963e278f37ccc055ca2
brandomarcel <33080093+brandomarcel@users.noreply.github.com>
2018-10-12 16:54:50 -0500

    Update SebastianTayo.java

--- src/Integrantes/SebastianTayo.java
+++ src/Integrantes/SebastianTayo.java
@@ -12,5 +12,6 @@ package Integrantes;
 public class SebastianTayo {
     public static void main(String[] args) {
         System.out.println("Hola mundo :v");
+        System.out.println("Hola mundo :v");
     }
 }
