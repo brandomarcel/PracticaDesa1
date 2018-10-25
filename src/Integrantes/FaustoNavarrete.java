fd064ac9e2e9a35975bf1aa3ab002878c2b064ec
Fausto <Fausto@Hernan>
2018-10-24 19:37:33 -0500

    prueb

--- src/Integrantes/FaustoNavarrete.java
+++ src/Integrantes/FaustoNavarrete.java
@@ -13,6 +13,8 @@ public class FaustoNavarrete {
     
     public static void main(String[] args) {
         System.out.println("hola");
+        System.out.println("hola");
+        
     }
     
 }
